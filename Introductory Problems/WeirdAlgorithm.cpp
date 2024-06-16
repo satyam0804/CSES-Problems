49f3d194ca5cc32876d459dca3a2305d410bbf51
Satyam Lakhera <75569926+satyam0804@users.noreply.github.com>
2024-06-16 15:15:36 +0530

    Initial

---
+++ Introductory Problems/WeirdAlgorithm.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define vlln vector<long long int>
+const int N = 2e5 + 10, MOD = 1e9 + 7;
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll n;
+    cin >> n;
+
+    while (n > 1)
+    {
+        cout << n << " ";
+        if (n % 2 == 0)
+        {
+            n = n / 2;
+        }
+        else
+        {
+            n = 3 * n + 1;
+        }
+    }
+
+    cout << 1 << endl;
+
+    return 0;
+}

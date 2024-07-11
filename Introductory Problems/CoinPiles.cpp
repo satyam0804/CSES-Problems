0a824ae333ef8f23056fdc2b4fd49b4fb6086f17
Satyam Lakhera <75569926+satyam0804@users.noreply.github.com>
2024-07-11 21:52:54 +0530

    Coin Piles

---
+++ Introductory Problems/CoinPiles.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define vlln vector<long long int>
+const int N = 2e5 + 10, MOD = 1e9 + 7;
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll a, b;
+        cin >> a >> b;
+
+        if ((2 * a - b) % 3 || (2 * a - b) < 0 || (2 * b - a) % 3 || (2 * b - a) < 0)
+        {
+            cout << "NO\n";
+        }
+        else
+        {
+            cout << "YES\n";
+        }
+    }
+
+    return 0;
+}
